079510c98ad067f12c321aee2a74256b81400769
Edward R <96d7cf74@opayq.com>
2018-01-13 19:31:23 -0500

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+// A React element with three arguements: element, properties, child content
+
+const title = React.createElement(
+  'h1', // The element we create
+  {id: 'title', className: 'header'},  //The properties of the elemnet
+  'Hello World!'    // Child content the element contains. Displays content inside of h1
+  )
+
+// Render the above element, uses two arguements
+ReactDOM.render(
+  title, // The name of the element to render
+  // Where we want to render the element to the DOM
+  document.getElementById('react-container')
+  )
